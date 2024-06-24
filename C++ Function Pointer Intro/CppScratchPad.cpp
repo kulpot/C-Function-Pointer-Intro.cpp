d88c0a8d400c5860c7b7cd62b14467bd60af9db5
Kulpot <kulpotwahu@gmail.com>
2024-06-25 04:16:05 +0800

    ,,

--- C++ Function Pointer Intro/CppScratchPad.cpp
+++ C++ Function Pointer Intro/CppScratchPad.cpp
@@ -4,7 +4,7 @@ using std::endl;
 
 //C++ Function Pointer Intro   //ref link:https://www.youtube.com/watch?v=y5BT3Wp5ETI&list=PLRwVmtr-pp05fEuxbVLY4HY8K1wMPo1ut
 
-void foo() {}
+void foo() {}		// an address 
 
 void main()
 {
