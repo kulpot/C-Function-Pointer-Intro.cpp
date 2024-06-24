fbd3c3233066e5689b77724a2d73540cadddafb7
Kulpot <kulpotwahu@gmail.com>
2024-06-24 15:30:20 +0800

    C++ Function Pointer Intro

--- C++ Function Pointer Intro/CppScratchPad.cpp
+++ C++ Function Pointer Intro/CppScratchPad.cpp
@@ -2,7 +2,7 @@
 using std::cout;
 using std::endl;
 
-//ref link:https://www.youtube.com/watch?v=y5BT3Wp5ETI&list=PLRwVmtr-pp05fEuxbVLY4HY8K1wMPo1ut
+//C++ Function Pointer Intro   //ref link:https://www.youtube.com/watch?v=y5BT3Wp5ETI&list=PLRwVmtr-pp05fEuxbVLY4HY8K1wMPo1ut
 
 void main()
 {
